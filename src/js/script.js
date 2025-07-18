b855b1a9e44b0b8cb577f4c5ed8352f7e9e0de17
Sviatoslav <svyatoslavdjs@gmail.com>
2025-07-18 21:42:16 +0300

    Make a Burger menu for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,19 @@ import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "../sass/style.scss";
 
+const burger = document.querySelector(".burger"),
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
+});
 
 try {
     // init Swiper:
