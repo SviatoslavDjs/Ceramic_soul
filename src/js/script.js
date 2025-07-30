d59615c8b89a33aa44c6aff64b6c71ad5396e0b1
Sviatoslav <svyatoslavdjs@gmail.com>
2025-07-30 20:14:37 +0300

    Implemented form submission, improved error handling behavior, and added animations to some elements

--- src/js/script.js
+++ src/js/script.js
@@ -118,10 +118,24 @@ try {
                 .querySelector('#checkbox')
                 .parentElement.parentElement.querySelector('.checkbox-error-message')
         })
+        .onSuccess((event) => {
+            const form = event.currentTarget;
+            const formData = new FormData(form);
+
+            fetch("https://httpbin.org/post", {
+                method: "POST",
+                body: formData,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("Success", data);
+                    form.reset();
+                });
+        });
 } catch (e) { }
 try {
-    const validator = new JustValidate('#footer-form');
-    validator
+    const validatorFooter = new JustValidate('#footer-form');
+    validatorFooter
 
         .addField('#email-footer', [
             {
